b0be30dd3736686e4ab1be6ce013ac0c71af0ad2
Tushar Rajput <tusharo17rajput@gmail.com>
2023-11-17 12:25:23 +0530

    initial commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link"
+import MaxWidthWrapper from "./MaxWidthWrapper"
+import { buttonVariants } from "./ui/button"
+
+const NavBar = () => {
+  return (
+    <nav className="sticky h-14 inset-x-1 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+          <Link href="/" className="flex z-40 font-semibold">
+            <span>
+              catche
+              <span className="text-blue-600">e.</span>
+            </span>
+          </Link>
+
+          <div className="hidden items-center space-x-4 sm:flex">
+            <>
+              <Link
+                href="/pricing"
+                className={buttonVariants({ variant: "ghost", size: "sm" })}
+              >
+                Pricing
+              </Link>
+            </>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  )
+}
+
+export default NavBar
